a215107e900732190bbeef04fa448669211f3288
vanninaa <86481315+vanninaa@users.noreply.github.com>
2021-07-29 16:14:02 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<style>body { background-color: gray }</style>
+</head>
+<body>
+<center>
+<font color="green"><h1>Hello BRO!!!</h1></font>
+
+<?php
+echo '<p style="color: gold">
+      This is AWS Elastic Beanstalk WebServer with PHP App by Jenkins</p>';
+echo '<br>Server IP:'.$_SERVER['SERVER_ADDR'];
+?>
+<br>
+Version 2.3 
+</center>
+</body>
+</html>
